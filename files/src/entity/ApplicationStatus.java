33af4ed6ab2a1c4170a73f0a4dc4f64f052ea6f6
kkllow <kimberly.leong05@gmail.com>
2025-03-24 22:53:01 +0800

    add entity

---
+++ files/src/entity/ApplicationStatus.java
@@ -0,0 +1,23 @@
+package entity;
+
+/**
+ * This enum represents the status of the application
+ */
+public enum ApplicationStatus {
+    /**
+     * The status of the application is pending
+     */
+    PENDING,
+    /**
+     * The status of the application is successful
+     */
+    SUCCESSFUL,
+    /**
+     * The status of the application is unsuccessful
+     */
+    UNSUCCESSFUL,
+    /**
+     * The status of the application is booked
+     */
+    BOOKED
+}
---
+++ files/src/entity/RegistrationStatus.java
@@ -0,0 +1,19 @@
+package entity;
+
+/**
+ * This enum represents the status of the registration
+ */
+public enum RegistrationStatus {
+    /**
+     * The status of the registration is pending
+     */
+    PENDING,
+    /**
+     * The status of the registration is approved
+     */
+    APPROVED,
+    /**
+     * The status of the registration is rejected
+     */
+    REJECTED
+}
