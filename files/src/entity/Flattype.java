43eaa2d04e653516956182f0f338eae1f8bc6368
kkllow <kimberly.leong05@gmail.com>
2025-03-24 22:56:56 +0800

    add enums

---
+++ files/src/entity/Flattype.java
@@ -0,0 +1,15 @@
+package entity;
+
+/**
+ * This enum represents the type of flat
+ */
+public enum Flattype {
+    /**
+     * The type of flat is tworoom flat
+     */
+    TWOROOM,
+    /**
+     * The type of flat is threeroom flat
+     */
+    THREEROOM
+}
